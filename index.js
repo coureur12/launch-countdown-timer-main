aedacd4eec3182aa28ee176c503bd8c3b8990c67
coureur12 <80371972+coureur12@users.noreply.github.com>
2022-02-03 04:29:10 +0100

    second commit

--- index.js
+++ index.js
@@ -1,1 +1,45 @@
-zebi
+setInterval(() => {
+var future = Date.parse("feb 06, 2022 01:30:00");
+var now = Date.now();
+ diff = future - now;
+
+ days = Math.floor(diff / (1000 * 60 * 60 * 24));
+ hours = Math.floor(diff/ (1000 * 60 * 60));
+ minutes = Math.floor(diff/ (1000 * 60));
+ secondes = Math.floor(diff/ 1000);
+
+ d = days;
+ h = hours - days * 24;
+ m = minutes - hours * 60;
+ s = secondes - minutes * 60;
+
+
+
+
+
+dateText = document.getElementById("day");
+if (d <10){
+    dateText.textContent = "0" + d;
+}else{
+    dateText.textContent = d;
+}
+hourText = document.getElementById("hours");
+if (h < 10){
+    hourText.textContent = "0" + h;
+}else{
+    hourText.textContent = h;
+}
+minuteText = document.getElementById("minutes");
+if (m < 10){
+    minuteText.textContent = "0" + m;
+}else{
+    minuteText.textContent = m;
+}
+secondeText = document.getElementById("seconds");
+if (s < 10 ){
+    secondeText.textContent = "0" + s;
+}else{
+    secondeText.textContent = s;
+}
+
+}, 1000);
